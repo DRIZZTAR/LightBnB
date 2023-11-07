85c49c0680c4bf2515d6e34368f18c969b06ae55
Tyson Skakun <tyson_jeremy@hotmail.com>
2023-11-07 15:43:17 -0700

    Change addUser function to query and insert into users database, tested user sign up, logout, then sign in working

--- LightBnB_WebApp-master/db/database.js
+++ LightBnB_WebApp-master/db/database.js
@@ -45,11 +45,25 @@ const getUserWithId = function (id) {
  * @return {Promise<{}>} A promise to the user.
  */
 
-const addUser = function (user) {
-  const userId = Object.keys(users).length + 1;
-  user.id = userId;
-  users[userId] = user;
-  return Promise.resolve(user);
+// const addUser = function (user) {
+//   const userId = Object.keys(users).length + 1;
+//   user.id = userId;
+//   users[userId] = user;
+//   return Promise.resolve(user);
+// };
+
+const addUser = function(user) {
+  const query = `
+    INSERT INTO users (name, email, password)
+    VALUES ($1, $2, $3)
+    RETURNING *;`
+
+    const values = [user.name, user.email, user.password];
+
+    return pool
+      .query(query, values)
+      .then(res => res.rows[0])
+      .catch(err => console.error('Querry error', err.stack))
 };
 
 /// Reservations
