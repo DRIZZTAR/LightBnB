a3b753347d963448d0a2e1abf93a619ac964f0c7
Tyson Skakun <tyson_jeremy@hotmail.com>
2023-11-08 15:19:55 -0700

    refactor the getAllReservations function to return appropriate data from our lightbnb database.

--- LightBnB_WebApp-master/db/database.js
+++ LightBnB_WebApp-master/db/database.js
@@ -69,13 +69,27 @@ const addUser = function(user) {
 /// Reservations
 
 /**
- * Get all reservations for a single user.
- * @param {string} guest_id The id of the user.
- * @return {Promise<[{}]>} A promise to the reservations.
+ * @param {string} guest_id The id of the user for whom to retrieve reservations.
+ * @param {number} [limit=10] The maximum number of reservations to return.
+ * @return {Promise<Array>} A promise that will resolve to an array of reservation objects.
  */
 const getAllReservations = function (guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+  const query = `
+    SELECT reservations.*, properties.*, AVG(property_reviews.rating) as average_rating
+    FROM reservations
+    JOIN properties ON reservations.property_id = properties.id
+    JOIN property_reviews ON properties.id = property_reviews.property_id
+    WHERE reservations.guest_id = $1
+    GROUP BY properties.id, reservations.id
+    ORDER BY reservations.start_date
+    LIMIT $2;
+  `;
+  return pool
+    .query(query, [guest_id, limit])
+    .then(res => res.rows)
+    .catch(err => console.error('Query error', err.stack));
 };
+
 
 /// Properties
 
